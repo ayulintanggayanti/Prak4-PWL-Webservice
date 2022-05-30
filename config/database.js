462428dd3ca869f16848af82c053f7f969c78be8
ayulintanggayanti <ayu.119140095@student.itera.ac.id>
2022-05-30 14:33:07 +0700

    update

--- config/database.js
+++ config/database.js
@@ -1,11 +1,10 @@
 const mySql = require('mysql');
 
 const connection = mySql.createConnection({
-    host: 'containers-us-west-52.railway.app',
+    host: 'containers-us-west-52.railway.app:7942',
     user: 'root',
     password: 'kyH4nTbHgvo6VUP2FCO4',
     database: 'railway',
-    port: '7942'
 });
 
 module.exports = connection;
